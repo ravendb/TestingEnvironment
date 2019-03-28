0967e7b3e108178d198dc39a8f0db1eaffbfb78d
egor <egor@ravendb.net>
2019-03-28 17:42:19 +0200

    Added BackupAndRestore Scenario [WIP]

---
+++ Scenarios/BackupAndRestore/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BackupAndRestore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            RunCountersScenarios(args[0]);
+        }
+
+        public static void RunCountersScenarios(string orchestratorUrl)
+        {
+            using (var client = new BackupAndRestore(orchestratorUrl, "BackupAndRestore"))
+            {
+                client.Initialize();
+                client.RunTest();
+            }
+        }
+    }
+}
