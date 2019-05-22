f3cdee3b602b34036bcdc6edaaef4166dbf48936
Egor <egor@ravendb.net>
2019-05-22 17:00:04 +0300

    Add TaskCleaner for backup tasks

---
+++ TaskCleaner/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace TaskCleaner
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            RunTaskCleanerScenario(args[0]);
+        }
+
+        public static void RunTaskCleanerScenario(string orchestratorUrl)
+        {
+            using (var client = new TaskCleaner(orchestratorUrl, "TaskCleaner"))
+            {
+                client.Initialize();
+                client.RunTest();
+            }
+        }
+    }
+}
