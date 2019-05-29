0089fb9aec42d4ddf976cc6e6ad88d21bd55a88c
aviviadi <avivi.adi.hr@gmail.com>
2019-05-29 16:20:16 +0300

    minors

--- zipAllProjects.sh
+++ zipAllProjects.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 rm -rf Zips >& /dev/null
 mkdir Zips
-for i in $(find . | grep TEOutLinux | sed 's/TEOutLinux.*/TEOutLinux/g' | sort | uniq | cut -d'/' -f2-99 | sed 's/TeAgent/TEAGENT/g' ); do tar cvfpz $(echo $i | sed 's/Agent\///g' | sed 's/Scenarios\///g' | cut -f1 -d'/' | sed 's/TEAGENT/TeAgent/g' ).tar.gz $i; done
+for i in $(find . | grep TEOutLinux | sed 's/TEOutLinux.*/TEOutLinux/g' | sort | uniq | cut -d'/' -f2-99 | sed 's/TeAgent/TEAGENT/g' ); do tar cvfpz Zips/$(echo $i | sed 's/Agent\///g' | sed 's/Scenarios\///g' | cut -f1 -d'/' | sed 's/TEAGENT/TeAgent/g' ).tar.gz $i; done
